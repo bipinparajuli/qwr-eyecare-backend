b4982b801247b262834229a96ebe4ef4bd5bf307
Ajinkya Puar <puar.ajinkya@gmail.com>
2021-10-15 14:03:25 +0530

    Event Model Update

--- models/event_model.js
+++ models/event_model.js
@@ -8,167 +8,171 @@ const Schema = mongoose.Schema;
 // @NOTE: geoJSon insert value like this { type: 'Point', coordinates: [-179.0, 0.0] }
 
 const EventSchema = new Schema({
+  //basic
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  datetime: {
+    type: String,
+    required: true,
+  },
+  game_mode: {
+    type: String,
+    required: false,
+  },
+  device_id: {
+    type: String,
+    required: true,
+  },
+  ip: {
+    type: String,
+  },
+  macaddress: {
+    type: String,
+  },
+  login: {
+    type: Boolean,
+    require: true,
+    default: true,
+  },
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    // required: true
+  },
+  //apptime
+  appstarttime: {
+    type: Date
+  },
+  appendtime: {
+    type: Date
+  },
+  appstayintime: {
+    type: Number
+  },
 
-    name: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    game_mode: {
-        type: String,
-        required: false
-    },
-    user_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        // required: true
-    },
-    datetime: {
-        type: String,
-        required: true
-    },
-    device_id: {
-        type: String,
-        required: true
-    },
-    // left_eye:{
-    //     type: Number,
-    // },
-    // right_eye:{
-    //     type: Number,
-    // },
-    stayintime: {
-        type: Number,
-        // required: true
-    },
-    eye: {
-        type: String,
-        // required: true
-    },
-    testid: {
-        type: String,
-        // required: true
-    },
-    gamemode: {
-        type: String,
-        // required: true
-    },
-    result: {
-        type: String,
-        // required: true
-    },
-    testCount: {
-        type: Number,
-        // required: true
-    },
-    rightcount: {
-        type: Number,
-        // required: true
-    },
-    falsecount: {
-        type: Number,
-        // required: true
-    },
-    level: {
-        type: Number,
-        // required: true
-    },
-    direction: {
-        type: String,
-        // required: true
-    },
-    Eid: {
-        type: String,
-        // required: true
-    },
-    eyetestid: {
-        type: String,
-        // required: true
-    },
-    chooseddirection: {
-        type: String,
-        // required: true
-    },
-    showedEid: {
-        type: String,
-        // required: true
-    },
-    requestjson: {
-        type: String,
-        // required: true
-    },
-    responsejson: {
-        type: String,
-        // required: true
-    },
-    textCount: {
-        type: Number,
-        // required: true
-    },
-    text: {
-        type: String,
-        // required: true
-    },
-    textID: {
-        type: String,
-        // required: true
-    },
-    aliresult: {
-        type: String,
-        // required: true
-    },
-    textreadcount: {
-        type: String,
-        // required: true
-    },
-    singletestid: {
-        type: String,
-        // required: true
-    },
-    sound: {
-        type: String,
-        // required: true
-    },
-    soundlenth: {
-        type: String,
-        // required: true
-    },
-    soundsize: {
-        type: String,
-        // required: true
-    },
-    alitext: {
-        type: String,
-        // required: true
-    },
-    message: {
-        type: String,
-        // required: true
-    },
-    lang: {
-        type: String,
-        // required: true
-    },
+  //game time
+  gamestayintime: {
+    type: Number,
+    // required: true
+  },
+  game_enterTime: {
+    type: Date,
+  },
+  game_exitTime: {
+    type: Date,
+  },
+  //Login
+  typephonetime: {
+    type: Number
+  },
+  optsendtostarttype: {
+    type: Number
+  },
+  otptypetime: {
+    type: Number
+  },
+  //////////////////////////////////////////////////games/////////////////////////////////////////////////////////
+  testid: {
+    type: String,
+  },
+  eye: {
+    type: String,
+  },
+  gamemode: {
+    type: String,
+  },
+  result: {
+    type: String,
+  },
+  testTime: {
+    type: Number,
+  },
+  testCount: {
+    type: Number,
+  },
+  rightcount: {
+    type: Number,
+  },
+  falsecount: {
+    type: Number,
+  },
 
-    // data: {
-    //     type: Object,
-    //     required: false,
-    //     default: ''
-    // },
+  ////////////////////singleTest///////////////
 
+  singletestid: {
+    type: String,
+  },
+  level: {
+    type: Number,
+  },
 
+  //////////ex1////////
+  direction: {
+    type: String,
+  },
+  chooseddirection: {
+    type: String,
+  },
+  e_direction: {
+    type: String
+  },
+  textCount: {
+    type: Number,
+  },
+  /////////////////ex2///////
+  text: {
+    type: String,
+  },
+  textreadcount: {
+    type: String,
+  },
+  alispeechtext: {
+    type: String,
+  },
+  alispeechrequesttime: {
+    type: String,
+  },
+  alispeechresponsetime: {
+    type: String,
+  },
+  alispeechresposejson: {
+    type: String,
+  },
+  sound: {
+    type: String,
+  },
+  soundlenth: {
+    type: String,
+  },
+  soundsize: {
+    type: String,
+  },
+
+  /////////////////////////////////////////lang
+  lang: {
+    type: String,
+  },
+
+  // data: {
+  //     type: Object,
+  //     required: false,
+  //     default: ''
+  // },
 }, {
-    strict: true,
-    timestamps: {
-        createdAt: "createdAt",
-        updatedAt: "updatedAt"
-    }
+  strict: true,
+  timestamps: {
+    createdAt: "createdAt",
+    updatedAt: "updatedAt",
+  },
 });
 
 EventSchema.on("index", (err) => {
-    console.log(">>>>>>>>>>>>>>>", err);
+  console.log(">>>>>>>>>>>>>>>", err);
 });
 module.exports = mongoose.model("Event", EventSchema);
